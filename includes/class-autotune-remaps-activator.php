6d7e9c297095089e5fe8480e0da49c72e7ba3690
Toby Wisener <toby.wisener@commscope.com>
2022-10-26 09:48:17 +0100

    Importing autotune's real site into the docker image and increasing max upload file size

--- includes/class-autotune-remaps-activator.php
+++ includes/class-autotune-remaps-activator.php
@@ -30,8 +30,8 @@ class Autotune_Remaps_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-		return; // Do not run the activate hook
-		
+		//return; // Do not run the activate hook
+
 		global $wpdb;
 		$table_name = $wpdb->prefix . "remaps";
 		$my_products_db_version = '1.0.0';
@@ -39,29 +39,29 @@ class Autotune_Remaps_Activator {
 
 		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$table_name}'" ) != $table_name ) {
 
-		    $sql = "CREATE TABLE $table_name ( 
-		    	`remap_id` INT NOT NULL AUTO_INCREMENT, 
-		    	`user_id` INT NOT NULL , 
-		    	`status` INT NOT NULL DEFAULT '0' , 
-		    	`manufacturer` VARCHAR(100) NOT NULL , 
-		    	`model` VARCHAR(100) NOT NULL , 
-		    	`year` YEAR NOT NULL , 
+		    $sql = "CREATE TABLE $table_name (
+		    	`remap_id` INT NOT NULL AUTO_INCREMENT,
+		    	`user_id` INT NOT NULL ,
+		    	`status` INT NOT NULL DEFAULT '0' ,
+		    	`manufacturer` VARCHAR(100) NOT NULL ,
+		    	`model` VARCHAR(100) NOT NULL ,
+		    	`year` YEAR NOT NULL ,
 		    	`engine_size` VARCHAR(100) NOT NULL,
-		    	`remap_file` TEXT NOT NULL , 
-		    	`ecu_type` TEXT NOT NULL , 
+		    	`remap_file` TEXT NOT NULL ,
+		    	`ecu_type` TEXT NOT NULL ,
 		    	`price` DECIMAL(13,2) NULL ,
-		    	`performance_tuning` BOOLEAN NOT NULL , 
-		    	`lambda_o2_decat` BOOLEAN NOT NULL , 
-		    	`dpf_removal` BOOLEAN NOT NULL , 
-		    	`adblue_scr_nox` BOOLEAN NOT NULL , 
-		    	`inlet_swirl_throttle` BOOLEAN NOT NULL , 
-		    	`egr_removal` BOOLEAN NOT NULL , 
-		    	`dtc` BOOLEAN NOT NULL , 
-		    	`dtc_p_codes` TEXT NULL , 
-		    	`other_notes` TEXT NULL , 
-		    	`created_at` DATETIME NOT NULL , 
+		    	`performance_tuning` BOOLEAN NOT NULL ,
+		    	`lambda_o2_decat` BOOLEAN NOT NULL ,
+		    	`dpf_removal` BOOLEAN NOT NULL ,
+		    	`adblue_scr_nox` BOOLEAN NOT NULL ,
+		    	`inlet_swirl_throttle` BOOLEAN NOT NULL ,
+		    	`egr_removal` BOOLEAN NOT NULL ,
+		    	`dtc` BOOLEAN NOT NULL ,
+		    	`dtc_p_codes` TEXT NULL ,
+		    	`other_notes` TEXT NULL ,
+		    	`created_at` DATETIME NOT NULL ,
 		    	`updated_at` DATETIME NULL ,
-		    	PRIMARY KEY `remap_id`(`remap_id`),		    	  
+		    	PRIMARY KEY `remap_id`(`remap_id`),
 		    	INDEX `user_id` (`user_id`)
 		    )	$charset_collate;";
 
@@ -72,21 +72,21 @@ class Autotune_Remaps_Activator {
 
 		/* Migration 18/02/2020
 		Toby: Make some columns nullable to handle storing of charges in the same table
-		
+
 		ALTER TABLE `wp_remaps` ADD `type` TINYINT NOT NULL DEFAULT '0' AFTER `user_id`;
 
-		ALTER TABLE `wp_remaps` 
-		CHANGE `manufacturer` `manufacturer` VARCHAR(100) NULL, 
-		CHANGE `model` `model` VARCHAR(100) NULL, CHANGE `year` `year` YEAR(4) NULL, 
-		CHANGE `engine_size` `engine_size` VARCHAR(100) NULL, 
+		ALTER TABLE `wp_remaps`
+		CHANGE `manufacturer` `manufacturer` VARCHAR(100) NULL,
+		CHANGE `model` `model` VARCHAR(100) NULL, CHANGE `year` `year` YEAR(4) NULL,
+		CHANGE `engine_size` `engine_size` VARCHAR(100) NULL,
 		CHANGE `remap_file` `remap_file` TEXT NULL,
-		CHANGE `ecu_type` `ecu_type` TEXT NULL, 
-		CHANGE `performance_tuning` `performance_tuning` TINYINT(1) NULL, 
-		CHANGE `lambda_o2_decat` `lambda_o2_decat` TINYINT(1) NULL, 
-		CHANGE `dpf_removal` `dpf_removal` TINYINT(1) NULL, 
-		CHANGE `adblue_scr_nox` `adblue_scr_nox` TINYINT(1) NULL, 
-		CHANGE `inlet_swirl_throttle` `inlet_swirl_throttle` TINYINT(1) NULL, 
-		CHANGE `egr_removal` `egr_removal` TINYINT(1) NULL, 
+		CHANGE `ecu_type` `ecu_type` TEXT NULL,
+		CHANGE `performance_tuning` `performance_tuning` TINYINT(1) NULL,
+		CHANGE `lambda_o2_decat` `lambda_o2_decat` TINYINT(1) NULL,
+		CHANGE `dpf_removal` `dpf_removal` TINYINT(1) NULL,
+		CHANGE `adblue_scr_nox` `adblue_scr_nox` TINYINT(1) NULL,
+		CHANGE `inlet_swirl_throttle` `inlet_swirl_throttle` TINYINT(1) NULL,
+		CHANGE `egr_removal` `egr_removal` TINYINT(1) NULL,
 		CHANGE `dtc` `dtc` TINYINT(1) NULL;
 		*/
 
---
+++ testing/wp_data/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:8:"disabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"lES6(eLe+bO9M:kMKrva<4WY S}#PQ&t[YDENPd4{z8T#4l_-gq>H<Dvj>fyT<!%";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:1857;s:11:"wafDisabled";b:0;}
